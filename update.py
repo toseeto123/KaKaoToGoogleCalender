fc1b8111f6849a6d8d22adba12e79edcc3eaaa05
toseeto123 <taipoone@gmail.com>
2023-01-08 23:10:16 +0900

    update.py 수정

--- update.py
+++ update.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from Google import Create_Service, convert_to_RFC_datetime
+import datetime
 
 CLIENT_SECRET_FILE='credentials.json'
 API_NAME='calendar'
@@ -8,22 +9,21 @@ SCOPES=['https://www.googleapis.com/auth/calendar']
 
 service=Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
 
+#시간 조종용 메소드
+hour_adjustment=-1
 # 캘린더 id는 구글캘린더 내부에서 직접적으로 확인도 가능함.
 calendar_id='8d762569ee30e088994ea3a39d894a289a99bdb4792c960ff6b9c220cfeb5dbe@group.calendar.google.com'
-
-#필요없는 조정 adjustment일수있음.
-hour_adjustment=-1
 event_request_body={
     'start': {
-      'dateTime': convert_to_RFC_datetime(2023,1,8,5 + hour_adjustment,30),
+      'dateTime': convert_to_RFC_datetime(2023,1,8,12 + hour_adjustment,30),
       'timeZone': 'Asia/Taipei'
     },
     'end':{
-        'dateTime': convert_to_RFC_datetime(2023,1,8,6 + hour_adjustment,30),
+        'dateTime': convert_to_RFC_datetime(2023,1,8,13 + hour_adjustment,30),
         'timeZone': 'Asia/Taipei'
     },
-    'summary': '가족식사',
-    'description': '부모님과 저녁먹기',
+    'summary': 'api update용',
+    'description': '파이썬으로 업데이트실행용',
     'colorId': 5,
     'status':'confirmed',
     'transparency': 'opaque',
@@ -31,7 +31,7 @@ event_request_body={
     'location' : 'seoul',
     'attendees':[
         {
-            'displayName':'JJ',
+            'displayName':'kim nam su',
             'comment':' I enjoy coding',
             'email': 'taipoone@naver.com',
             'optional': False,
@@ -56,25 +56,19 @@ response =service.events().insert(
 ).execute()
 
 pprint(response)
+
 ###update 구간
 
 eventId=response['id']
 
 ###update 구간 코드
-start_dateTime= convert_to_RFC_datetime(2023 , 1 , 8, 5 + hour_adjustment, 30)
-end_dateTime= convert_to_RFC_datetime(2023 , 1 , 8, 6 + hour_adjustment, 30)
+start_dateTime= convert_to_RFC_datetime(2023 , 1 , 5 ,18 + hour_adjustment,30)
+end_dateTime= convert_to_RFC_datetime(2023 , 1 , 6, 20 + hour_adjustment, 30)
 response['start']['dateTime']=start_dateTime
 response['end']['dateTime']=end_dateTime
-response['summary'] = '수정업데이트 222'
-response['Description']='업데이트실행완료'
+response['summary'] = '바비파티로 수정 업데이트'
+response['Description']='파뤼파뤼투나잇'
 service.events().update(
     calendarId=calendar_id,
     eventId=eventId,
     body=response).execute()
-
-## delete 구간 코드
-
-service.events().delete(
-    calendarId=calendar_id,
-    eventId=eventId).execute()
-
