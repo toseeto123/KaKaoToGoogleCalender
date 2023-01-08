8e6f4cc1f4817934bad37169d554405b3d58e7fb
김남수 <117258283+toseeto123@users.noreply.github.com>
2023-01-08 10:57:52 +0900

    insert,update,delete,list 기능 포함
    4가지 기능 포함 업데이트

--- test.py
+++ test.py
@@ -9,8 +9,92 @@ SCOPES=['https://www.googleapis.com/auth/calendar']
 service=Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
 
 # 캘린더 id는 구글캘린더 내부에서 직접적으로 확인도 가능함.
-calendar_id='72ac6aa0bd79505df40eb6cda729f6796b37b0059b984174518c7c9fbc951204@group.calendar.google.com'
+calendar_id='8d762569ee30e088994ea3a39d894a289a99bdb4792c960ff6b9c220cfeb5dbe@group.calendar.google.com'
 
-# create an event
-colors = service.colors().get().execute()
-pprint(colors)
+# # create an event
+# colors = service.colors().get().execute()
+# pprint(colors)
+
+#필요없는 조정 adjustment일수있음.
+hour_adjustment=-1
+event_request_body={
+    'start': {
+      'dateTime': convert_to_RFC_datetime(2023,1,5,12 + hour_adjustment,30),
+      'timeZone': 'Asia/Taipei'
+    },
+    'end':{
+        'dateTime': convert_to_RFC_datetime(2023,1,5,12 + hour_adjustment,30),
+        'timeZone': 'Asia/Taipei'
+    },
+    'summary': '가족식사',
+    'description': '부모님과 저녁먹기',
+    'colorId': 5,
+    'status':'confirmed',
+    'transparency': 'opaque',
+    'visibility': 'private',
+    'location' : 'seoul',
+    'attendees':[
+        {
+            'displayName':'JJ',
+            'comment':' I enjoy coding',
+            'email': 'taipoone@naver.com',
+            'optional': False,
+            'organizer': True,
+            'responseStatus':'accepted'
+        }
+    ]
+}
+
+maxAttendees=5
+sendNotification=True
+sendUpdate='none'
+supportsAttachments=True
+
+response =service.events().insert(
+    calendarId=calendar_id,
+    maxAttendees=maxAttendees,
+    sendNotifications=sendNotification,
+    sendUpdates=sendUpdate,
+    supportsAttachments=supportsAttachments,
+    body=event_request_body
+).execute()
+
+pprint(response)
+
+###update 구간
+
+eventId=response['id']
+
+###update 구간 코드
+start_dateTime= convert_to_RFC_datetime(2023 , 1 , 5 ,18 + hour_adjustment,30)
+end_dateTime= convert_to_RFC_datetime(2023 , 1 , 6, 20 + hour_adjustment, 30)
+response['start']['dateTime']=start_dateTime
+response['end']['dateTime']=end_dateTime
+response['summary'] = '바비파티로 수정 업데이트'
+response['Description']='파뤼파뤼투나잇'
+service.events().update(
+    calendarId=calendar_id,
+    eventId=eventId,
+    body=response).execute()
+
+#list 이벤트 구간
+response = service.calendarList().list(
+        maxResults=250,
+        showDeleted=False,
+        showHidden=False
+).execute()
+
+calendarItems = response.get('items')
+nextPageToken = response.get('nextPageToken')
+
+while nextPageToken :
+    response = service.calendarList().list(
+        maxResults=250,
+        showDeleted=False,
+        showHidden=False,
+        pageToken=nextPageToken
+    ).execute()
+    calendarItems = extend(response.get('items'))
+    nextPageToken = response.get('nextPageToken')
+
+print(calendarItems)
